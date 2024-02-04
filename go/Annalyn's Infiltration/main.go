b751df6608f5a3c0a73a7c9ef558cc2056339c39
pctmoraes <paula.ct.moraes@gmail.com>
2024-02-04 14:24:23 -0300

    Remove commented lines in main.go

--- go/Annalyn's Infiltration/main.go
+++ go/Annalyn's Infiltration/main.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main(){
-	// var knightIsAwake = false
-	// fmt.Println(annalyn.CanFastAttack(knightIsAwake))
+	var knightIsAwake = false
+	fmt.Println(annalyn.CanFastAttack(knightIsAwake))
 
-	// var knightIsAwake = true
-	// var archerIsAwake = true
-	// var prisonerIsAwake = true
-	// fmt.Println(annalyn.CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake))
+	var knightIsAwake = true
+	var archerIsAwake = true
+	var prisonerIsAwake = true
+	fmt.Println(annalyn.CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake))
 
-	// var archerIsAwake = true
-	// var prisonerIsAwake = true
-	// fmt.Println(annalyn.CanSignalPrisoner(archerIsAwake, prisonerIsAwake))
+	var archerIsAwake = true
+	var prisonerIsAwake = true
+	fmt.Println(annalyn.CanSignalPrisoner(archerIsAwake, prisonerIsAwake))
 
 	var knightIsAwake = false
 	var archerIsAwake = true
--- go/Cars Assemble/main.go
+++ go/Cars Assemble/main.go
@@ -6,14 +6,11 @@ import (
 )
 
 func main() {
-	// fmt.Println(cars.CalculateWorkingCarsPerHour(1547, 90))
-	// fmt.Println(cars.CalculateWorkingCarsPerMinute(1105, 90))
+	fmt.Println(cars.CalculateWorkingCarsPerHour(1547, 90))
 
+	fmt.Println(cars.CalculateWorkingCarsPerMinute(1105, 90))
 
 	fmt.Println(cars.CalculateCost(37))
-	// => 355000
 	
 	fmt.Println(cars.CalculateCost(21))
-	// => 200000
-
 }
